4a9dd40ba982ac60097651103bcd3fb2e92f8017
Gareth Ellis <gareth.ellis0@gmail.com>
2018-07-14 09:42:48 -0700

    WIP: Started Working Through The Final Navier Stokes Equations

--- src/ControlVolume.cpp
+++ src/ControlVolume.cpp
@@ -1,15 +1,28 @@
 #include "ControlVolume.h"
 
 using namespace units::literals;
+using namespace units::length;
+using namespace units::velocity;
+using namespace units::acceleration;
+using namespace units::time;
+using namespace units::pressure;
+using namespace units::density;
+using namespace units::viscosity;
+using namespace units::math;
 
-ControlVolume::ControlVolume() : pressure(0), velocity({0_m / 1_s, 0_m / 1_s}) {}
+ControlVolume::ControlVolume()
+  : pressure(0), velocity({0_m / 1_s, 0_m / 1_s}) {}
 
 ControlVolume::ControlVolume(
     std::pair<ControlVolume, Point2d> original_volume_with_point,
     std::pair<ControlVolume, Point2d> left_neighbour_with_point,
     std::pair<ControlVolume, Point2d> right_neighbour_with_point,
     std::pair<ControlVolume, Point2d> top_neighbour_with_point,
-    std::pair<ControlVolume, Point2d> bottom_neighbour_with_point) {
+    std::pair<ControlVolume, Point2d> bottom_neighbour_with_point,
+    second_t dt,
+    kg_per_cu_m density,
+    meters_squared_per_s viscosity
+) {
     // TODO: Should these conventions be somewhere else? Maybe put in README?
     /**
      * CONVENTIONS:
@@ -32,23 +45,31 @@ ControlVolume::ControlVolume(
     std::tie(t_neighbour, t_point)         = top_neighbour_with_point;
     std::tie(b_neighbour, b_point)         = bottom_neighbour_with_point;
 
+    meters_per_second_t v_old = original_vol.velocity.x;
+    meters_per_second_t w_old = original_vol.velocity.y;
+
     // TODO: Declare explicit types for each of these, will help catch errors
-    auto v_dot_x = (r_neighbour.velocity.x - original_vol.velocity.x) /
-                     (r_point.x - original_point.x);
-    auto w_dot_w = (t_neighbour.velocity.y - original_vol.velocity.y) /
-                     (t_point.y - original_point.y);
-    auto rho_dot_x =
+    auto v_dot_x = (r_neighbour.velocity.x - v_old) / (r_point.x - original_point.x);
+    auto w_dot_y = (t_neighbour.velocity.y - w_old) / (t_point.y - original_point.y);
+    auto p_dot_x =
         (r_neighbour.pressure - original_vol.pressure) / (r_point.x - original_point.x);
-    auto rho_dot_y =
+    auto p_dot_y =
         (t_neighbour.pressure - original_vol.pressure) / (t_point.y - original_point.y);
-    auto v_dotdot_x =
-        (l_neighbour.velocity.x - 2 * original_vol.velocity.x +
-         r_neighbour.velocity.x) /
-        ((original_point.x - l_point.x) * (r_point.x - original_point.x));
-    auto w_dotdot_y =
-        (b_neighbour.velocity.y - 2 * original_vol.velocity.y +
-         t_neighbour.velocity.y) /
-        ((original_point.y - l_point.y) * (r_point.y - original_point.y));
+    auto v_dotdot_x = (l_neighbour.velocity.x - 2 * v_old + r_neighbour.velocity.x) /
+                      ((original_point.x - l_point.x) * (r_point.x - original_point.x));
+    auto v_dotdot_y = (b_neighbour.velocity.x - 2 * v_old + t_neighbour.velocity.x) /
+                      ((original_point.y - b_point.y) * (t_point.y - original_point.y));
+    auto w_dotdot_x = (l_neighbour.velocity.x - 2 * v_old + r_neighbour.velocity.x) /
+            ((original_point.x - l_point.x) * (r_point.x - original_point.x));
+    auto w_dotdot_y = (b_neighbour.velocity.y - 2 * w_old + t_neighbour.velocity.y) /
+                      ((original_point.y - b_point.y) * (t_point.y - original_point.y));
+
+    this->velocity.x = v_old - dt * (v_dot_x + w_dot_y) * v_old -
+                              dt / density * p_dot_x +
+                              dt * viscosity * (v_dotdot_x + v_dotdot_y);
+    this->velocity.y = w_old - dt * (v_dot_x + w_dot_y) * w_old -
+                              dt / density * p_dot_y +
+                              dt * viscosity * (w_dotdot_x + w_dotdot_y);
 }
 
 ControlVolume::ControlVolume(double pressure, Velocity2d velocity)
